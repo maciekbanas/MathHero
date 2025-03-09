712f43c74528e8e730481077bf550ae5c1068857
Maciej Banas <banasmaciek@gmail.com>
2025-03-09 23:24:19 +0100

    Add a grid for map, change moving logic (from square to square), set position of  heroes, enemies and berries in squares.

--- src/items.py
+++ src/items.py
@@ -1,13 +1,23 @@
-import pygame
-import random
 import os
 from constants import *
 from utils import get_asset_path
 
-# Ładowanie grafiki jagód
 berry_image = pygame.image.load(get_asset_path(os.path.join("items", "berries.png")))
 berry_image = pygame.transform.scale(berry_image, (50, 50))
 
-# Funkcja generująca jagódki na planszy
-def generate_berries():
-    return [pygame.Rect(random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50), 30, 30) for _ in range(2, 4)]
+class Berry:
+    """Represents a berry on the map that restores health when collected."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.transform.scale(berry_image, (50, 50))
+        self.rect = pygame.Rect(self.x, self.y, 50, 50)
+
+    def draw(self, screen):
+        """Draws the berry on the screen."""
+        screen.blit(self.image, (self.x, self.y))
+
+    def check_collision(self, player):
+        """Checks if the player has collected the berry."""
+        return self.rect.colliderect(pygame.Rect(player.x, player.y, 50, 50))
