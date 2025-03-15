857dfa902675e759fcb612ea44a13890557df121
Maciej Banas <banasmaciek@gmail.com>
2025-03-15 15:42:39 +0100

    Change figure sprites for realms.

--- src/player.py
+++ src/player.py
@@ -46,7 +46,7 @@ class Player:
         self.character = character
         self.battle_sprite = pygame.transform.scale(player_sprites[character], (200, 200))
         self.sprite = load_gif_frames(player_minifig_sprites[character], new_size=(grid_size, grid_size))
-        self.realm_sprite = pygame.transform.scale(player_sprites[character], (100, 100))
+        self.realm_sprite = pygame.transform.scale(pygame.image.load(player_minifig_sprites[character]), (100, 100))
         self.animation = AnimatedSprite(self.sprite, frame_duration=100)
 
         self.grid_size = grid_size
